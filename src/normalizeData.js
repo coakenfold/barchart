41525fe979650b2a1b3cfd84a45675d001d46ba1
Chad Oakenfold <web@oakenfold.ca>
2021-01-22 21:46:43 -0800

    Adds data normalization

---
+++ src/normalizeData.js
@@ -0,0 +1,31 @@
+export const getMinMax = (data) => {
+  return data.reduce(({ min, max }, { volume: cur }) => {
+    if (min === undefined) {
+      return { min: cur, max: cur };
+    }
+    if (min === cur || max === cur) {
+      return { min, max };
+    }
+    if (min > cur) {
+      return { max, min: cur };
+    }
+    if (max < cur) {
+      return { min, max: cur };
+    }
+    return { min, max };
+  }, {});
+};
+export const getRange = ({ min, max }) => {
+  return max - min;
+};
+const normalizeData = (data) => {
+  const { min, max } = getMinMax(data);
+  const range = getRange({ min, max });
+  const normalized = data.map((cur) => {
+    console.log({ range, cur, min });
+    cur.normalized = (cur.volume - min) / range;
+    return cur;
+  });
+  return normalized;
+};
+export default normalizeData;
